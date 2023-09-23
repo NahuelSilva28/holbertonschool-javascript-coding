0f9c082d9d4dfdb013e9177a5029a572d3d182f9
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-09-23 19:53:38 +0000

    2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+if (!url) {
+  console.error('Usage: ./2-statuscode.js <URL>');
+  process.exit(1);
+}
+
+request(url, (error, response) => console.log(`code: ${response ? response.statusCode : 'Error'}`));
