e4a03b9a60341dde521ab3ba3fc5c3279f5c470c
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-09-23 20:36:25 +0000

    4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const request = require('request');
-const apiUrl = process.argv[2];
+const url = 'http://localhost:3000/';
 
-request(apiUrl, (error, response, body) => {
+request(url, (error, response, body) => {
   if (!error) {
-    const films = JSON.parse(body).results;
+    const films = JSON.parse(body);
     const characterId = 18;
     const count = films.reduce((total, film) => {
       if (film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)) {
