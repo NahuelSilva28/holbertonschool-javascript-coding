523fb27d60a5bf4ccee3b830e616eb6e0e08853c
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-09-23 19:17:53 +0000

    0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,10 +4,16 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
+if (!filePath) {
+  console.error('Usage: ./0-readme.js <file_path>');
+  process.exit(1);
+}
+
 fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    console.error('Error:', err);
+    console.error(err);
+    process.exit(2);
   } else {
-    console.log('File Content:', data);
+    console.log(data.trim());
   }
 });
