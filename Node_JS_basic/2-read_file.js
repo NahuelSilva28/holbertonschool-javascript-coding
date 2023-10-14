d50391439ff8f3339ea1000540d11e5378c41b3f
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-10-14 18:35:47 +0000

    2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -9,14 +9,16 @@ function countStudents(path) {
     lines.forEach((line) => {
       const [firstName, field] = line.split(',');
       data[field] = data[field] || { count: 0, names: [] };
-      data[field].count++;
+      data[field].count += 1; // Fixed the '++' operator
       data[field].names.push(firstName);
     });
 
     console.log(`Number of students: ${lines.length}`);
-    for (const field in data) {
+
+    // Use Object.keys(data) to iterate over properties
+    Object.keys(data).forEach((field) => {
       console.log(`Number of students in ${field}: ${data[field].count}. List: ${data[field].names.join(', ')}`);
-    }
+    });
   } catch (error) {
     throw new Error('Cannot load the database');
   }
